d1c8ad2fc25a3cdc5faab7c6fe11e2345df94705
Ajith PM <ajithpmdwaraka@gmail.com>
2025-02-22 14:21:57 +0530

    Client App Structured

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   return (
     <>
       <h1>Book App</h1>
+      <div>
+        <input type="text" id="bookInput" placeholder="Enter book title" />
+        <input type='number' id="bookInput2" placeholder="Enter book published year" />
+      </div>
     </>
   )
 }
